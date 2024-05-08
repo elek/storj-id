dfd6212c40cbfb4fe7214c8f374f85ee39ebdeee
Márton Elek <elek@apache.org>
2024-05-08 12:07:19 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/elek/storj-id
+
+go 1.22.1
+
+require (
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d
+	storj.io/common v0.0.0-20240325183111-3a1a80390ccd
+	storj.io/drpc v0.0.33
+)
+
+require (
+	github.com/calebcase/tmpfile v1.0.3 // indirect
+	github.com/flynn/noise v1.0.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/jtolio/noiseconn v0.0.0-20230111204749-d7ec1a08b0b8 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
+	github.com/spacemonkeygo/monkit/v3 v3.0.22 // indirect
+	github.com/zeebo/blake3 v0.2.3 // indirect
+	github.com/zeebo/errs v1.3.0 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,180 @@
+package main
+
+import (
+	"context"
+	"encoding/base32"
+	"encoding/base64"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"golang.org/x/exp/maps"
+	"os"
+	"sort"
+	"storj.io/common/base58"
+	"storj.io/common/identity"
+	"storj.io/common/peertls/tlsopts"
+	"storj.io/common/rpc"
+	"storj.io/common/storj"
+	"storj.io/drpc"
+	"strings"
+	"time"
+)
+
+func main() {
+	err := run()
+	if err != nil {
+		panic(err)
+	}
+}
+
+var base32Encoding = base32.StdEncoding.WithPadding(base32.NoPadding)
+var pathEncoding = base32.NewEncoding("abcdefghijklmnopqrstuvwxyz234567").WithPadding(base32.NoPadding)
+
+func run() error {
+	source := os.Args[1]
+
+	decodings := map[string]func(string) ([]byte, error){
+		"remote-id": getRemoteID,
+		"base58": func(s string) ([]byte, error) {
+			result, _, err := base58.CheckDecode(s)
+			return result, err
+		},
+		"base32": base32Encoding.DecodeString,
+		"auth-base32": func(s string) ([]byte, error) {
+			return base32Encoding.DecodeString(strings.ToUpper(s))
+		},
+		"base64": base64.URLEncoding.DecodeString,
+		"hex":    hex.DecodeString,
+		"path":   pathEncoding.DecodeString,
+		"file":   readFromFile,
+	}
+
+	encodings := map[string]func([]byte) string{
+		"hex":    hex.EncodeToString,
+		"base58": base58.Encode,
+		"nodeid": func(bytes []byte) string {
+			fromBytes, err := storj.NodeIDFromBytes(bytes)
+			if err != nil {
+				return err.Error()
+			}
+			return fromBytes.String()
+		},
+		"nodeurl": func(bytes []byte) string {
+			if !strings.Contains(source, ":") {
+				return ""
+			}
+			fromBytes, err := storj.NodeIDFromBytes(bytes)
+			if err != nil {
+				return err.Error()
+			}
+			return fromBytes.String() + "@" + source
+		},
+		"base32": base32Encoding.EncodeToString,
+		"base64": base64.URLEncoding.EncodeToString,
+		"path":   pathEncoding.EncodeToString,
+	}
+
+	keys := maps.Keys(decodings)
+	sort.Strings(keys)
+	for _, id := range keys {
+		decoded, err := decodings[id](source)
+		if err == nil {
+			fmt.Printf("Using %s as %s\n", source, id)
+			ekeys := maps.Keys(encodings)
+			sort.Strings(ekeys)
+			for _, ei := range ekeys {
+				decoded := encodings[ei](decoded)
+				if decoded != "" {
+					fmt.Println("  ", ei, decoded)
+				}
+			}
+
+		}
+	}
+
+	return nil
+}
+
+func readFromFile(s string) ([]byte, error) {
+	_, err := os.Stat(s)
+	if err != nil {
+		return nil, err
+	}
+	id, err := identity.NodeIDFromCertPath(s)
+	if err != nil {
+		return nil, err
+	}
+	return id.Bytes(), nil
+}
+
+func getRemoteID(hostPort string) ([]byte, error) {
+	if !strings.Contains(hostPort, ":") {
+		return nil, errors.New("doesn't look like a host:port")
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	tlsOptions, err := createEphemeralTLSIdentity(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	dialer := rpc.NewDefaultDialer(tlsOptions)
+	dialer.Pool = rpc.NewDefaultConnectionPool()
+
+	dialer.DialTimeout = 10 * time.Second
+	// TODO
+	//dialContext := socket.BackgroundDialer().DialContext
+	//
+	////lint:ignore SA1019 it's safe to use TCP here instead of QUIC + TCP
+	//dialer.Connector = rpc.NewDefaultTCPConnector(&rpc.ConnectorAdapter{DialContext: dialContext}) //nolint:staticcheck
+
+	conn, err := dialer.DialAddressInsecure(ctx, hostPort)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = conn.Close() }()
+	in := struct{}{}
+	out := struct{}{}
+	_ = conn.Invoke(ctx, "asd", &NullEncoding{}, in, out)
+	peerIdentity, err := conn.PeerIdentity()
+	if err != nil {
+		return nil, err
+	}
+
+	return peerIdentity.ID.Bytes(), nil
+}
+
+func createEphemeralTLSIdentity(ctx context.Context) (*tlsopts.Options, error) {
+	ident, err := identity.NewFullIdentity(ctx, identity.NewCAOptions{
+		Difficulty:  0,
+		Concurrency: 1,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	tlsConfig := tlsopts.Config{
+		UsePeerCAWhitelist: false,
+		PeerIDVersions:     "0",
+	}
+
+	tlsOptions, err := tlsopts.NewOptions(ident, tlsConfig, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return tlsOptions, nil
+}
+
+type NullEncoding struct {
+}
+
+func (n NullEncoding) Marshal(msg drpc.Message) ([]byte, error) {
+	return []byte{1}, nil
+}
+
+func (n NullEncoding) Unmarshal(buf []byte, msg drpc.Message) error {
+	return nil
+}
+
+var _ drpc.Encoding = &NullEncoding{}
